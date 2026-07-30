27091f30790f7bf533b481545f3fe4f012c85f76
Felype Gabryel <felype.g.ferreira@gmail.com>
2026-07-30 13:55:39 -0300

    formatado os arquivos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ListaProdutosComponent } from './lista-produtos/lista-produtos.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ListaProdutosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
