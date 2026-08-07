322dec6d2144ca9cb10f97f11794c77c1695e7a0
Thyago <thyago.v.santos6@aluno.senai.br>
2026-08-07 16:12:32 -0300

    certo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { ListaprodutosComponent } from './listaprodutos/listaprodutos.component';
 
-
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [ListaprodutosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- src/app/listaprodutos/listaprodutos.component.ts
+++ src/app/listaprodutos/listaprodutos.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-listaprodutos',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './listaprodutos.component.html',
   styleUrl: './listaprodutos.component.css'
 })
 export class ListaprodutosComponent {
 
+  produtos: string[] = [
+    '1 - Pão - 4,50',
+    '1 - Manteiga - 16,00',
+    '1 - Café - 12,80'
+  ];
+
+  adicionarProduto() {
+    const descricao = (document.getElementById('descricao') as HTMLInputElement).value;
+    const valor = (document.getElementById('valor') as HTMLInputElement).value;
+
+    if (descricao === '' || valor === '') {
+      alert('Preencha todos os campos!');
+      return;
+    }
+
+    this.produtos.push(`1 - ${descricao} - ${valor}`);
+  }
+
+  limparTudo() {
+    this.produtos = [];
+  }
 }
